3fe809aa152337cfedf9773302bce36b26e8c3d5
AndrewWhiteZ <ksarion1@gmail.com>
2022-05-06 02:42:44 +0500

    0.2.0

--- Assets/Controllers/MainSceneController.cs
+++ Assets/Controllers/MainSceneController.cs
@@ -8,10 +8,9 @@ using UnityEngine.SceneManagement;
 
 public class MainSceneController : MonoBehaviour
 {	
-	//public Question[] questions;
-
 	public List<Question> questions = new List<Question>();
 	public List<Answer> answers = new List<Answer>();
+	public List<Product> products = new List<Product>();
 
 	public GameObject Date; //Объект отображения внутриигрового времени
 	public GameObject Score; //Объект отображения денежного счёта игрока
@@ -25,6 +24,9 @@ public class MainSceneController : MonoBehaviour
 	public GameObject AnswerText2Panel;
 	public GameObject AnswerButton1;
 	public GameObject AnswerButton2;
+	public GameObject PropertyRightButton1;
+	public GameObject PropertyRightButton2;
+	public GameObject Release;
 
 	public float secPerDays = 0.5f; //Число реальных секунд приходящихся на один игровой день
 	
@@ -37,6 +39,11 @@ public class MainSceneController : MonoBehaviour
 	static int week = 1;
 	static int day = 1;
 	float timer = 0;
+
+	public string productName = "NameCop";
+	public bool askNewQuestionFlag = false;
+	public bool releaseFlag = false;
+	public int askedQuestionNumber = 0;
 	
 	public static int moneyScore = 50000; //денежный счёт игрока
 	public static int monthlyExpenses = 4000; //месячные затраты
@@ -65,6 +72,17 @@ public class MainSceneController : MonoBehaviour
 				day++;
 				if(day >= MaxDay)
 				{
+					if(askNewQuestionFlag)
+					{
+						AskQuestion();
+						askNewQuestionFlag = false;
+					}
+					
+					if (releaseFlag)
+					{
+						Release.SetActive(true);
+						releaseFlag = false;
+					}
 					week++;
 					if(week >= MaxWeek)
 					{
@@ -114,6 +132,11 @@ public class MainSceneController : MonoBehaviour
 			Score.GetComponent<TMPro.TextMeshProUGUI>().color = new Color (1,0,0,1);
 		}
 	}
+
+	public void SetAskNewQuestion()
+	{
+		askNewQuestionFlag = true;
+	}
 	
 	void InputOutputCheck()
 	{
@@ -123,24 +146,12 @@ public class MainSceneController : MonoBehaviour
 			ContextMenu.SetActive(!flag);
 			ContextMenu.transform.position = Input.mousePosition;
 		}
-	} 
-	
-	public void FirstStage()
-	{
-		var c = 0;
-		if (c < 3)
-		{
-			var weekBeforeAsking = week;
-			if(week == weekBeforeAsking) AskQuestion();
-			c++;
-			
-		}
 	}
 
 	public void AskQuestion()
 	{		
+		askedQuestionNumber++;
 		var randomQuestion = questions[new System.Random().Next(0, questions.Count)];
-		//Question randomQuestion = new Question("Что бы вы съели?", "Пельмени", "Вареники", 1.4f, 0.9f);
 		
 		QuestionTextPanel.GetComponent<TMPro.TextMeshProUGUI>().text = randomQuestion.QuestionText;
 		AnswerText1Panel.GetComponent<TMPro.TextMeshProUGUI>().text = randomQuestion.AnswerText1;
@@ -148,19 +159,58 @@ public class MainSceneController : MonoBehaviour
 		
 		QuestionPanel.SetActive(true);
 	}
-	
+
 	public void ReadAnswer()
 	{
 		if (AnswerButton1.activeSelf)
 		{
-			answers.Add(new Answer(QuestionTextPanel.GetComponent<TMPro.TextMeshProUGUI>().text, AnswerText1Panel.GetComponent<TMPro.TextMeshProUGUI>().text, 1.3f));
+			answers.Add(new Answer(QuestionTextPanel.GetComponent<TMPro.TextMeshProUGUI>().text,
+				AnswerText1Panel.GetComponent<TMPro.TextMeshProUGUI>().text, 1.3f));
 		}
 		else
 		{
-			answers.Add(new Answer(QuestionTextPanel.GetComponent<TMPro.TextMeshProUGUI>().text, AnswerText2Panel.GetComponent<TMPro.TextMeshProUGUI>().text, 0.7f));
+			answers.Add(new Answer(QuestionTextPanel.GetComponent<TMPro.TextMeshProUGUI>().text,
+				AnswerText2Panel.GetComponent<TMPro.TextMeshProUGUI>().text, 0.7f));
+		}
+
+		if (askedQuestionNumber >= 5)
+		{
+			askNewQuestionFlag = false;
+			releaseFlag = true;
+			askedQuestionNumber = 0;
+		}
+		else
+		{
+			askNewQuestionFlag = true;
 		}
 		QuestionPanel.SetActive(false);
-		//FirstStage();
+	}
+
+	public void ReleaseProduct()
+	{
+		if (PropertyRightButton1.activeSelf)
+		{
+			products.Add(new Product(productName, GetProfitRatio(), 10000, PropertyRights.SOLD_TO_ANOTHER_COMPANY));
+			moneyScore += 25000;
+			SetScore();
+		}
+		else
+		{
+			products.Add(new Product(productName, GetProfitRatio(), 10000, PropertyRights.BELONGS_TO_OUR_COMPANY));
+			moneyScore += 5000;
+			SetScore();
+		}
+	}
+
+	float GetProfitRatio()
+	{
+		var a = 1.0f;
+		foreach (var answer in answers)
+		{
+			a *= answer.AnswerProfitRatio;
+		}
+
+		return a;
 	}
 }
 
@@ -194,4 +244,26 @@ public class Answer
 		this.AnswerText = AnswerText;
 		this.AnswerProfitRatio = AnswerProfitRatio;
 	}
+}
+
+public class Product
+{
+	public string ProductName;
+	public float ProductProfitRatio;
+	public int ProductCost;
+	public PropertyRights PropertyRight;
+
+	public Product(string ProductName, float ProductProfitRatio, int ProductCost, PropertyRights PropertyRight)
+	{
+		this.ProductName = ProductName;
+		this.ProductProfitRatio = ProductProfitRatio;
+		this.ProductCost = ProductCost;
+		this.PropertyRight = PropertyRight;
+	}
+}
+
+public enum PropertyRights
+{
+	SOLD_TO_ANOTHER_COMPANY,
+	BELONGS_TO_OUR_COMPANY
 }
